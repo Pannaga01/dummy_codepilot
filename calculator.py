c8403fc8fba0fc43bbc62030cc8c5968a6155a6b
Pannaga01 <53700154+Pannaga01@users.noreply.github.com>
2026-05-22 11:44:56 +0530

    created bug

--- calculator.py
+++ calculator.py
@@ -5,7 +5,7 @@ ef calculate(num1, operator, num2=None):
         return num1 - num2
     elif operator == '*':
         return num1 * num2
-    elif operator == '/':
+    elif operator == '/'
         if num2 == 0:
             return "Error: Division by zero"
         return num1 / num2
@@ -15,7 +15,7 @@ ef calculate(num1, operator, num2=None):
         return "Error: Invalid operator"
 
 if __name__ == "__main__":
-    print(f"5 + 3 = {calculate(5, '+', 3)}")
+    print(f"5 + 3 = {calculate(5, '+', 3)})
     print(f"10 - 4 = {calculate(10, '-', 4)}")
     print(f"6 * 7 = {calculate(6, '*', 7)}")
     print(f"10 / 2 = {calculate(10, '/', 2)}")
